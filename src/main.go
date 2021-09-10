d11c197512fd4edf865c01b7a75f6372e8c95b56
Rogério Rocha <rogeriosilvarocha@gmail.com>
2021-09-10 07:38:37 -0300

    main rest github wh ok

--- src/main.go
+++ src/main.go
@@ -31,7 +31,7 @@ func setupRouter() *gin.Engine {
 		{
 			
 			examples.GET("hello", func(c *gin.Context) {
-				c.JSON(http.StatusOK, "Roger")
+				c.JSON(http.StatusOK, "Roger via github")
 			})
 		}
 	}
