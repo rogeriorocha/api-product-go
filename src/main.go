0632061e3caa1f2f97e105068be16c84cf556a5a
Rogério Rocha <rogeriosilvarocha@gmail.com>
2021-09-11 02:21:44 -0300

    test

--- src/main.go
+++ src/main.go
@@ -34,7 +34,7 @@ func setupRouter() *gin.Engine {
 
 			examples.GET("hello", func(c *gin.Context) {
 
-				c.JSON(http.StatusOK, "k8s")
+				c.JSON(http.StatusOK, "JotaM")
 			})
 
 			examples.GET("error", func(c *gin.Context) {
